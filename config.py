7840d469aac8721aab195759583657d8f322a660
buytfdip <buytfdip@gmail.com>
2022-08-24 02:22:35 -0400

    config.py update

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 API_KEY = "YOUR PUBLIC KEY GOES HERE"
 API_SECRET = "YOUR PRIVATE KEY GOES HERE"
 BASE_URL = "https://paper-api.alpaca.markets" #use "https://api.alpaca.markets" for live brokerage account
-DB_FILE =  "Data/app.db"
+DB_FILE =  "Databases/app.db"
